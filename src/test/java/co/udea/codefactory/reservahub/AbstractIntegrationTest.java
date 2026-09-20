1f2548ebf5078b4e5c78c7ea382de3f83f7c4550
Arge2004 <arge2004@example.com>
2026-09-20 18:42:22 -0500

    Refactor integration test setup by moving PostgreSQL container configuration to a new TestcontainersConfiguration class

--- src/test/java/co/udea/codefactory/reservahub/AbstractIntegrationTest.java
+++ src/test/java/co/udea/codefactory/reservahub/AbstractIntegrationTest.java
@@ -1,14 +1,7 @@
 package co.udea.codefactory.reservahub;
 
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.springframework.context.annotation.Import;
 
-@Testcontainers
+@Import(TestcontainersConfiguration.class)
 public abstract class AbstractIntegrationTest {
-
-	@Container
-	@ServiceConnection
-	static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine");
 }
